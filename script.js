ecbb7de83b8ae154fda01fdfbb4250303321bea1
ElderEmoScrolls <christopher.s.carter01@gmail.com>
2026-03-11 17:53:05 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,92 @@
+fetch("findings.json")
+.then(response => response.json())
+.then(data => {
+
+const findings = data.findings
+
+const severityCount = {
+Critical:0,
+High:0,
+Medium:0,
+Low:0,
+Info:0
+}
+
+const timeline = {}
+
+findings.forEach(f => {
+
+if(severityCount[f.severity] !== undefined){
+severityCount[f.severity]++
+}
+
+if(!timeline[f.date]){
+timeline[f.date]=0
+}
+
+timeline[f.date]++
+
+})
+
+createSeverityChart(severityCount)
+createTimelineChart(timeline)
+populateTable(findings)
+
+})
+
+function createSeverityChart(data){
+
+new Chart(
+document.getElementById("severityChart"),
+{
+type:"bar",
+data:{
+labels:Object.keys(data),
+datasets:[{
+label:"Findings",
+data:Object.values(data)
+}]
+}
+}
+)
+
+}
+
+function createTimelineChart(data){
+
+new Chart(
+document.getElementById("timelineChart"),
+{
+type:"line",
+data:{
+labels:Object.keys(data),
+datasets:[{
+label:"Findings Discovered",
+data:Object.values(data)
+}]
+}
+}
+)
+
+}
+
+function populateTable(findings){
+
+const table = document.querySelector("#findingsTable tbody")
+
+findings.forEach(f => {
+
+const row = document.createElement("tr")
+
+row.innerHTML = `
+<td>${f.severity}</td>
+<td>${f.title}</td>
+<td>${f.asset}</td>
+<td>${f.date}</td>
+`
+
+table.appendChild(row)
+
+})
+
+}
